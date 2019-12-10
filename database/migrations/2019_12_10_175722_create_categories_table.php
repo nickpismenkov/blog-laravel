80b3c3b4b604e08d9ae3e3e3c3c7ffa6016ce283
Nikolay Pismenkov <npismenkov@mail.ru>
2019-12-10 21:42:36 +0300

    '#4'

---
+++ database/migrations/2019_12_10_175722_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->integer('parent_id')->nullable();
+            $table->tinyInteger('published')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->integer('modified_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,51 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+
+<div class="container">
+	
+	@component('admin.components.breadcrumb')
+		@slot('title') Список категорий @endslot
+		@slot('parent') Главная @endslot
+		@slot('active') Категории @endslot
+	@endcomponent
+
+	<hr>
+
+	<a href="{{ route('admin.category.create') }}" class="btn btn-primary mb-3"><i class="fa fa-plus-square-o"></i> Создать категорию</a>
+
+	<table class="table table-striped">
+		<thead>
+			<th>Наименование</th>
+			<th>Публикация</th>
+			<th class="text-right">Действие</th>
+		</thead>
+		<tbody>
+			@forelse($categories as $category)
+				<tr>
+					<td>{{ $category->title }}</td>
+					<td>{{ $category->published }}</td>
+					<td>
+						<a href="{{ route('admin.category.edit', ['id' => $category->id]) }}"><i class="fa fa-edit"></i></a>
+					</td>
+				</tr>
+			@empty
+				<tr>
+					<td colspan="3" class="text-center"><h2>Данные отсутствуют</h2></td>
+				</tr>
+			@endforelse
+		</tbody>
+		<tfoot>
+			<tr>
+				<td colspan="3">
+					<ul class="pagination">
+						{{ $categories->links() }}
+					</ul>
+				</td>
+			</tr>
+		</tfoot>
+	</table>
+
+</div>
+
+@endsection
